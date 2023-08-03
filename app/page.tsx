d367e0457b5162e69500ed87d3e117eff7e96c9c
Will Moritz <willthefirst@gmail.com>
2023-08-02 22:13:13 -0700

    WAY better UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,31 @@
-import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 
 export default function Home() {
   return (
-    <main className="font-serif flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-xl" aria-details="There, i ESCAPED the single quote ESLint, are you happy?">
-        Will&rsquo;s Website
-      </h1>
-      <label htmlFor='password'>Please enter your password:
-      </label>
-      <input type="password" id="password" name="password" maxLength={25} />
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Welcome</CardTitle>
+          <CardDescription>Let&apos;s get started.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Password</Label>
+                <Input id="name" placeholder="Please enter your password" />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button>Log in</Button>
+        </CardFooter>
+      </Card>
     </main>
   )
 }
