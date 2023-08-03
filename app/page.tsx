ac1980fef49bf3a8161e7df12008f576ead4f31d
Will Moritz <willthefirst@gmail.com>
2023-08-02 22:18:30 -0700

    Get ready to handle first interaction

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { MouseEventHandler } from 'react'
 
 export default function Home() {
+  const handleClick: MouseEventHandler<HTMLButtonElement> = (event): void => {
+    throw new Error('Function not implemented.')
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <Card className="w-[350px]">
@@ -22,7 +28,7 @@ export default function Home() {
           </form>
         </CardContent>
         <CardFooter className="flex justify-between">
-          <Button>Log in</Button>
+          <Button onClick={handleClick}>Log in</Button>
         </CardFooter>
       </Card>
     </main>
