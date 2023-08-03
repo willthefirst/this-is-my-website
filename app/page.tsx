11d2a8827cf2852f5e21a0df0c1034a9f801f770
Will Moritz <willthefirst@gmail.com>
2023-08-03 01:12:25 -0700

    Nicer display behavior

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { ChangeEventHandler, MouseEventHandler, useState } from 'react'
+import { ChangeEventHandler, FormEventHandler, MouseEventHandler, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { errorCountSlice, selectErrorCount } from '@/lib/redux'
 import AlertPicker from './AlertPicker'
@@ -12,18 +12,27 @@ import AlertPicker from './AlertPicker'
 export default function Home() {
   const dispatch = useDispatch()
   const [password, setPassword] = useState("")
+  const [isUserTrying, setIsUserTrying] = useState(false)
   const errorCount: number = useSelector(selectErrorCount)
+
+  const handleFocus = () => {
+    setIsUserTrying(true)
+  }
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
     setPassword(event.target.value)
   }
 
-  const handleClick: MouseEventHandler<HTMLButtonElement> = () => {
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    console.log("yo")
     dispatch(errorCountSlice.actions.increment());
+    setIsUserTrying(false)
     setPassword("")
   }
 
   const hasError = errorCount > 0;
+  const isAlertVisible = hasError && !isUserTrying
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
@@ -31,27 +40,29 @@ export default function Home() {
         <CardHeader>
           <CardTitle>Welcome!</CardTitle>
         </CardHeader>
-        <CardContent>
-          <form>
+        <form onSubmit={handleSubmit}>
+
+          <CardContent>
             <div className="grid w-full items-center gap-4">
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" placeholder="Please enter your password" onChange={handleChange} value={password} />
+                <Input id="password" type="password" placeholder="Please enter your password" onChange={handleChange} onFocus={handleFocus} value={password} />
               </div>
             </div>
-          </form>
-        </CardContent>
-        <CardFooter className="flex flex-col space-y-6">
-          <div>
-            <Button onClick={handleClick}>Log in</Button>
-          </div>
-          {
-            hasError &&
+          </CardContent>
+          <CardFooter className="flex flex-col space-y-6">
             <div>
-              <AlertPicker errorCount={errorCount} />
+              <Button type="submit">Log in</Button>
             </div>
-          }
-        </CardFooter>
+            {
+              isAlertVisible &&
+              <div>
+                <AlertPicker errorCount={errorCount} />
+              </div>
+            }
+          </CardFooter>
+        </form>
+
       </Card>
     </main >
   )
