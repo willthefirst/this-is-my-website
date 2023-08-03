45cefa2b4d5c7286173cf7554dafa820569eb554
Will Moritz <willthefirst@gmail.com>
2023-08-03 00:33:54 -0700

    Clear password when it's wrong

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,23 @@ import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { MouseEventHandler } from 'react'
+import { ChangeEventHandler, MouseEventHandler, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { errorCountSlice, selectErrorCount } from '@/lib/redux'
 import AlertPicker from './AlertPicker'
 
 export default function Home() {
   const dispatch = useDispatch()
+  const [password, setPassword] = useState("")
   const errorCount: number = useSelector(selectErrorCount)
 
-  const handleClick: MouseEventHandler<HTMLButtonElement> = (event): void => {
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = () => {
     dispatch(errorCountSlice.actions.increment());
+    setPassword("")
   }
 
   const hasError = errorCount > 0;
@@ -29,8 +35,8 @@ export default function Home() {
           <form>
             <div className="grid w-full items-center gap-4">
               <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Password</Label>
-                <Input id="name" placeholder="Please enter your password" />
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" placeholder="Please enter your password" onChange={handleChange} value={password} />
               </div>
             </div>
           </form>
