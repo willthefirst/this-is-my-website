134da741c64318ca715a88e5ed38c6a6f1c8eeef
Will Moritz <willthefirst@gmail.com>
2023-08-02 21:55:41 -0700

    I don't like this, needs to be more normal

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 aria-details="There, i ESCAPED the single quote ESLint, are you happy?">
-        Will&lsquo;s Website
+    <main className="font-serif flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-xl" aria-details="There, i ESCAPED the single quote ESLint, are you happy?">
+        Will&rsquo;s Website
       </h1>
-
+      <label htmlFor='password'>Please enter your password:
+      </label>
+      <input type="password" id="password" name="password" maxLength={25} />
     </main>
   )
 }
