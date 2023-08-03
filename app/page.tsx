d876ec318f0f57085515b387bf3496ab259e4daa
Will Moritz <willthefirst@gmail.com>
2023-08-02 21:39:33 -0700

    Nice indents cuz

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 aria-details="There, i ESCAPED the single quote ESLint, are you happy?">Will&lsquo;s Website</h1>
+      <h1 aria-details="There, i ESCAPED the single quote ESLint, are you happy?">
+        Will&lsquo;s Website
+      </h1>
+
     </main>
   )
 }
